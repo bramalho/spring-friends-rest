a32a7f81364ee738a46e809580a421a1b96b169d
Bruno Ramalho <bramalho@divante.pl>
2021-04-02 13:12:42 +0200

    init

---
+++ src/test/java/com/bruno/FriendsREST/FriendsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.FriendsREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
